2af448f058333d25b237153791bdfeb33b7eb91f
ASHDKP <141820983+ASHDKP@users.noreply.github.com>
2023-08-09 16:56:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 streamlit.title("My Parent's New healthy Diner")
-streamlit.text("Omega 3 & blueberry Oatmeal")
-streamlit.text("Kale, Spinach Smoothie")
-streamlit.text("Hard-boiled Free Range Egg")
-streamlit.text("Avocado Toast")
+streamlit.text("🥣 Omega 3 & blueberry Oatmeal")
+streamlit.text("🥗  Kale, Spinach Smoothie")
+streamlit.text("🐔 Hard-boiled Free Range Egg")
+streamlit.text("🥑Avocado Toast🍞")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
