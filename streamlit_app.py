4cb0183531ea306c26df9607506d12320c46a17a
ASHDKP <141820983+ASHDKP@users.noreply.github.com>
2023-08-09 16:31:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title("Welcome to Ashwin's diner")
 streamlit.text("Today's Menu")
-streamlit.text("Anda Pulao")
-streamlit.text("Fried Papad")
+streamlit.text("Hot Chai")
+streamlit.text("Farsaaan")
