9a760dd821a7dd33c88c1a3e4140e5c19023bb0f
ASHDKP <141820983+ASHDKP@users.noreply.github.com>
2023-08-09 17:13:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text("🐔 Hard-boiled Free Range Egg")
 streamlit.text("🥑Avocado Toast🍞")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
 
