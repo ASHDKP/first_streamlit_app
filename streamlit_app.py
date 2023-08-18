46400aab2a84f35340433c71e386339e6226493f
ASHDKP <141820983+ASHDKP@users.noreply.github.com>
2023-08-18 09:25:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #Normalize the JSON response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Convert the nnormalized response into a dataframe 
