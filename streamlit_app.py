9788aae8421ec35c29c2fe8ec96e0f27c7f25f76
ASHDKP <141820983+ASHDKP@users.noreply.github.com>
2023-08-18 17:48:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def get_fruityvice_data (this_fruit_choice) :
       streamlit.write('The user entered ', this_fruit_choice)
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+      return fruityvice_normalized
 
 #New section to display FruityVice API results
 streamlit.header("Fruityvice Fruit Advice!")
